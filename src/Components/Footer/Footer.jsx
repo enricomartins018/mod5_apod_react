3953eaccfa023b3c5b687fcacd9f8e3ca91bec5d
enricomartins018 <enrico.martins018@gmail.com>
2022-08-29 19:44:33 -0300

    feat: Footer finalizado

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import S from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={S.footer}>
         <p>Todos os direitos reservados.</p>
         <p>Para fins educacionais</p>
     </footer>
