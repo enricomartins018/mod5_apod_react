76643a8900a493944b9062a70d330f7571a83f90
Matheus-Hen <matheushlsilva2000@gmail.com>
2022-08-29 19:49:01 -0300

    Fetch salvo

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Body = () => {
+  const [dados, setDados] = useState([])
+  async function APINasa(){
+   await fetch('https://api.nasa.gov/planetary/apod?api_key=brh2yVXhuHZSXnpDXg38AcoI60lyYzx5i4Hfm70v')
+    .then(Response => Response.json() )
+    .then(data => setDados(data))
+  }
+  useEffect(()=>{
+    APINasa()
+    console.log(dados);
+  },[])
   return (
     <body>
         <section>
+            
             <button>Navegar</button>
         </section>
     </body>
